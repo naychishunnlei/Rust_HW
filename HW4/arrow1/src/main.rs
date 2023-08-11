c3d54e2b6373a0ae2087d5ef62a1be73d6912aac
naychishunnlei <89850504+naychishunnlei@users.noreply.github.com>
2023-08-11 10:50:19 +0700

    Update main.rs

--- HW4/arrow1/src/main.rs
+++ HW4/arrow1/src/main.rs
@@ -1,3 +1,4 @@
+//ordinary loop
 fn make_arrow1(size: i32) -> String {
     let mut arrow = String::new();
 
@@ -18,12 +19,42 @@ fn make_arrow1(size: i32) -> String {
     arrow
 }
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let size_arg = if args.len() < 2 {""} else {&args[1]};
-    let size: i32 = size_arg.parse().unwrap_or(0);
+//recursion
+fn make_arrow1_recur(size: i32, row_index: i32) -> String {
+    if row_index >= size * 2 {
+        return String::new();
+    }
 
-    let arrow1 = make_arrow1(size);
+    let mut arrow = String::new();
+    let star_index = if row_index <= size {
+        row_index
+    } else {
+        size * 2 - row_index
+    };
+
+    star_generator(star_index, &mut arrow);
+    arrow.push('\n');
+    arrow.push_str(&make_arrow1_recur(size, row_index + 1));
+
+    arrow
+}
+
+fn star_generator(star_index: i32, arrow: &mut String) {
+    if star_index > 0 {
+        arrow.push('*');
+        star_generator(star_index - 1, arrow);
+    }
+}
+
+fn main() {
+    println!("Using ordinary loop:");
+    let arrow1_size: i32 = 5;
+    let arrow1 = make_arrow1(arrow1_size);
+    print!("{}", arrow1);
+
+    println!("Using recursion:");
+    let arrow1_size: i32 = 5;
+    let arrow1 = make_arrow1_recur(arrow1_size, 1);
     print!("{}", arrow1);
 }
 
@@ -36,5 +67,25 @@ fn test_make_arrow1() {
         make_arrow1(5),
         "*\n**\n***\n****\n*****\n****\n***\n**\n*\n"
     );
+}
+
+#[test]
+fn test_make_arrow1_recur() {
+    let star = 3;
+    let expected = "*\n**\n***\n**\n*\n";
+    let arrow = make_arrow1_recur(star, 1);
+
+    assert_eq!(expected, arrow);
+}
+
+#[test]
+fn test_arrow1_star_generator_with_zero() {
+    let star_num = 0;
+    let mut arrow = String::new();
+    let expected = "";
+
+    star_generator(star_num, &mut arrow);
+
+    assert_eq!(expected, arrow);
 }
 
